78e49ea24d1a890f9a33797afb52b02d13b7a05a
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2025-02-10 10:49:14 +0100

    add initial set of packages

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y tmux 
+dnf install -y neovim virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-manager virt-viewer libguestfs-tools python3-libguestfs virt-top edk2-ovmf swtpm tailscale 
 
 # Use a COPR Example:
 #
@@ -22,3 +22,5 @@ dnf install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable libvirtd.service
+systemctl enable tailscaled.service
