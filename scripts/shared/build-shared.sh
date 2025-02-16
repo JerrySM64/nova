2758e78035bd45b4b264effe91ba48a3343a0b45
Alxhr0 <alxhr0@proton.me>
2025-02-16 22:02:43 +0100

    Fix codecs

--- scripts/shared/build-shared.sh
+++ scripts/shared/build-shared.sh
@@ -8,7 +8,7 @@ dnf5 -y copr enable sentry/kernel-blu
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 wget https://download.opensuse.org/repositories/home:Alxhr0/Fedora_41/home:Alxhr0.repo -P /etc/yum.repos.d
 
-rpm-ostree override remove libavdevice-free ffmpeg-free libavcodec-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free --install ffmpeg
+dnf5 -y install ffmpeg
 dnf5 -y install intel-media-driver
 
 # Kernel BLU
