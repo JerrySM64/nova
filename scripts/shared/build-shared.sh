4f038879e3f1074c87fdce74353d0df6ed969f57
Alxhr0 <alxhr0@proton.me>
2025-02-16 22:40:01 +0100

    Fix build-shared.sh syntax error

--- scripts/shared/build-shared.sh
+++ scripts/shared/build-shared.sh
@@ -8,7 +8,7 @@ dnf5 -y copr enable sentry/kernel-blu
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 wget https://download.opensuse.org/repositories/home:Alxhr0/Fedora_41/home:Alxhr0.repo -P /etc/yum.repos.d
 
-if grep -q "nova_plasma" /usr/share/nova/image_type || grep -q "nova_plasma_dx" /usr/share/nova/image_type; || grep -q "nova_plasma_gaming" /usr/share/nova/image_type; || grep -q "supernova_plasma" /usr/share/nova/image_type; then
+if grep -q "nova_plasma" /usr/share/nova/image_type || grep -q "nova_plasma_dx" /usr/share/nova/image_type || grep -q "nova_plasma_gaming" /usr/share/nova/image_type || grep -q "supernova_plasma" /usr/share/nova/image_type; then
     rpm-ostree override remove libavdevice-free ffmpeg-free libavcodec-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free --install ffmpeg
 else
     dnf5 -y install ffmpeg
