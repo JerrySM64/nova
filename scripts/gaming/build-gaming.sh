6e790cc6fdf1e5f195f8ddf68eb8d383fe0c152b
Alxhr0 <alxhr0@proton.me>
2025-02-16 22:11:52 +0100

    Fix dnf5 copr error

--- scripts/gaming/build-gaming.sh
+++ scripts/gaming/build-gaming.sh
@@ -2,9 +2,6 @@
 
 set -ouex pipefail
 
-
-dnf5 -y copr enable sentry/kernel-blu
-dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
 
 
