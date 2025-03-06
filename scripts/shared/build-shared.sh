85c1fa07c9d8baad1275962c3f946178dcabb62d
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2025-03-06 18:05:33 +0100

    Hoping to finally make it shut up.

--- scripts/shared/build-shared.sh
+++ scripts/shared/build-shared.sh
@@ -17,7 +17,7 @@ wget https://download.opensuse.org/repositories/home:Alxhr0/Fedora_41/home:Alxhr
 #if grep -q "nova_plasma" /usr/share/nova/image_type || grep -q "nova_plasma_dx" /usr/share/nova/image_type || grep -q "nova_plasma_gaming" /usr/share/nova/image_type || grep -q "supernova_plasma" /usr/share/nova/image_type; then
     rpm-ostree override remove libpostproc-free libswresample-free libavcodec-free libavformat-free libavutil-free libavfilter-free libswscale-free --install ffmpeg
 #else
-    dnf5 -y install ffmpeg
+#    dnf5 -y install ffmpeg
 #fi
 
 #rpm-ostree override remove libavdevice-free ffmpeg-free libavcodec-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free --install ffmpeg
